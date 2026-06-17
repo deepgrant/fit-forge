5af2a9e064e3dee379376e5d4bd533f70c6fef3d
Grant Mills <dormice_manlike_0r@icloud.com>
2026-06-17 07:40:08 -0400

    [ref FF-001] clean up junk. Changes to FIT file codec check.

---
+++ source/scala/fitforge/fit/FitLayout.scala
@@ -0,0 +1,17 @@
+package fitforge.fit
+
+/** A format/layout summary of a FIT file: how many of each message type, and totals. */
+final case class FitLayout(counts: Vector[(String, Int)], totalMessages: Int, totalFields: Int)
+
+object FitLayout {
+
+  /** Summarise a file by message type, ordered by global message number. */
+  def of(file: FitFile): FitLayout = {
+    val counts = file.messages
+      .groupBy(_.globalNum)
+      .toVector
+      .sortBy { case (num, _) => num }
+      .map { case (num, msgs) => (FitProfile.Mesg.nameOf(num), msgs.size) }
+    FitLayout(counts, file.messages.size, file.messages.map(_.fields.size).sum)
+  }
+}
